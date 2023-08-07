6af67c413343b8feb8c2a3a305d0397fb9ea27f1
Philipp Lackner <mail@pl-coding.com>
2023-08-07 17:13:51 +0200

    Providing StandardDispatchers

--- core/domain/src/main/java/com/ivy/core/domain/di/DomainModuleDI.kt
+++ core/domain/src/main/java/com/ivy/core/domain/di/DomainModuleDI.kt
@@ -1,10 +1,18 @@
 package com.ivy.core.domain.di
 
+import com.ivy.core.domain.pure.util.DispatcherProvider
+import com.ivy.core.domain.pure.util.StandardDispatchers
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DomainModuleDI {
+
+    @Provides
+    fun provideDispatcherProvider(): DispatcherProvider {
+        return StandardDispatchers()
+    }
 }
