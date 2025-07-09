446ce39ec26e2ca8d1bbcab26f2fb87545ece1a3
Juan Gaines <juandavidgaines@gmail.com>
2025-07-08 22:01:14 -0400

    Update compileSdkVersion and targetSdkVersion to 35
    This commit updates the Android `compileSdkVersion` and `targetSdkVersion`
    to 35. It also adds a suppression for the unsupported compileSdk
    warning related to API 35.

--- buildSrc/src/main/java/com/ivy/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/ivy/buildsrc/dependencies.kt
@@ -29,12 +29,12 @@ object Project {
     const val versionCode = 117
 
     //Compile SDK & Build Tools
-    const val compileSdkVersion = 34
+    const val compileSdkVersion = 35
 
     //App
     const val applicationId = "com.ivy.wallet"
     const val minSdk = 28
-    const val targetSdk = 34
+    const val targetSdk = 35
 }
 
 object Versions {
